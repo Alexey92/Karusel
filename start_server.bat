53d43db74bdb54e80f2cb74f195052d395c8cbe8
Alex <medina_aleksis@mail.ru>
2026-06-03 19:53:12 +0300

    start server bat update

--- start_server.bat
+++ start_server.bat
@@ -1,6 +1,6 @@
 @echo off
-set KARUSEL_CLOUD_URL=http://localhost:5050/api/event
-set KARUSEL_CLOUD_KEY=local-test-key
+set KARUSEL_CLOUD_URL=http://192.168.0.149:5050/api/event
+set KARUSEL_CLOUD_KEY=IznGRaUKRunLmiLAizbiKL7xYcCYyuD4AiGXlzV8tgI
 set KARUSEL_LOCATION_ID=1
 cd /d C:\karusel\server
 python main.py
