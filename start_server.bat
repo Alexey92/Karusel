0eb12d627ac0cf71be6c51033e2545387833238b
Alex <medina_aleksis@mail.ru>
2026-06-05 15:35:36 +0300

    clean catalog, add readme

--- start_server.bat
+++ start_server.bat
@@ -1,6 +1,6 @@
 @echo off
 set KARUSEL_CLOUD_URL=http://192.168.0.149:5050/api/event
-set KARUSEL_CLOUD_KEY=g9OjH1yklR1SGt5xh1-XfY6ltNZ1CEJYJNjABQ8Ft0s
-set KARUSEL_LOCATION_ID=1
+set KARUSEL_CLOUD_KEY=FxlgMagFiUvhOaPJmv2giN85zqtrG58OQIF1FtPQ9vY
+set KARUSEL_LOCATION_ID=2
 cd /d C:\karusel\server
 python main.py
