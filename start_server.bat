fee1b57d3e1fcf2d170d237d0e7fcc1d8f11162b
Alex <medina_aleksis@mail.ru>
2026-06-08 17:16:28 +0300

    update start server

--- start_server.bat
+++ start_server.bat
@@ -1,5 +1,5 @@
 @echo off
-set KARUSEL_CLOUD_URL=http://192.168.0.149:5050/api/event
+set KARUSEL_CLOUD_URL=http://194.186.104.79:5050/api/event
 set KARUSEL_CLOUD_KEY=hsD031z6OKV4Wlx3snP5xQUsvQFpPB0DZsogPkaJLB0
 set KARUSEL_LOCATION_ID=1
 cd /d C:\karusel\server
